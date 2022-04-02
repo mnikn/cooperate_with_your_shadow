a0f327c59d6388a912cf0534050d868b0188eabb
mnikn <mnikn@outlook.com>
2022-04-02 23:12:27 +0800

    feat: add audio

--- src/sprites/Player.gd
+++ src/sprites/Player.gd
@@ -1,1 +1,28 @@
 extends "res://src/sprites/BaseSprite.gd"
+
+
+var footstep_effect = preload("res://assets/audio/footstep.ogg")
+var jump_effect = preload("res://assets/audio/jump.ogg")
+var jump_falldown_effect = preload("res://assets/audio/jump_falldown.ogg")
+
+func _ready():
+	footstep_effect.loop = false
+	jump_effect.loop = false
+	jump_falldown_effect.loop = false
+
+func _physics_process(delta):
+	if self.is_on_floor() and $SoundEffectPlayer.stream == jump_effect:
+		if $SoundEffectPlayer.stream != jump_falldown_effect:
+			$SoundEffectPlayer.stream = jump_falldown_effect
+		if not $SoundEffectPlayer.playing:
+			$SoundEffectPlayer.play()
+	if self.velocity.y < 0:
+		if $SoundEffectPlayer.stream != jump_effect:
+			$SoundEffectPlayer.stream = jump_effect
+		if not $SoundEffectPlayer.playing:
+			$SoundEffectPlayer.play()
+	if self.is_on_floor() and self.velocity.x != 0:
+		if $SoundEffectPlayer.stream != footstep_effect:
+			$SoundEffectPlayer.stream = footstep_effect
+		if not $SoundEffectPlayer.playing:
+			$SoundEffectPlayer.play()
