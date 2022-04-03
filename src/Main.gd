432b5c780df16e220f006cabd13cc0d78ee9016d
mnikn <mnikn@outlook.com>
2022-04-03 13:16:35 +0800

    feat: add banner

--- src/Main.gd
+++ src/Main.gd
@@ -1,1 +1,6 @@
 extends Node2D
+
+
+func _input(event):
+	if Input.is_action_pressed("ui_accept"):
+		SceneChanger.change_scene("res://src/levels/Level1.tscn")
--- src/levels/Level1.gd
+++ src/levels/Level1.gd
@@ -2,7 +2,7 @@ extends "res://src/levels/BaseLevel.gd"
 
 func _ready():
 	if not GameManager.flags.level1_hint:
-		yield(self.get_tree().create_timer(1), "timeout")
+		yield(self.get_tree().create_timer(0.3), "timeout")
 		$Mask.visible = true
 		$Popups/Dialogue.show_content("You need to cooperte with your shadow, triggered all trigger to open the door. But  beacuse is your shadow, so your shadow DELAY IS INEVITABLE! ")
 		yield($Popups/Dialogue, "finished")
