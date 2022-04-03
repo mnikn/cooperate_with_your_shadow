53e627143fbd461463e34c7a2a128edc2fdf18b2
mnikn <mnikn@outlook.com>
2022-04-03 14:17:46 +0800

    feat: add spike

--- src/components/PausePanel.gd
+++ src/components/PausePanel.gd
@@ -9,3 +9,10 @@ func _on_Restart_pressed():
 	self.hide()
 	SceneChanger.change_scene(self.get_tree().current_scene.filename)
 	self.get_tree().paused = false
+
+
+func _on_Title_pressed():
+	self.hide()
+	SceneChanger.change_scene("res://src/Main.tscn")
+	self.get_tree().paused = false
+	
